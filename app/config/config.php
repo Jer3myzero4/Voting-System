5aad2afe3ef82ac8198ba1da853d802b6da3f920
Jeremy Festin <bsitjeremyfestin@gmail.com>
2025-11-25 11:30:04 +0800

    ads

--- app/config/config.php
+++ app/config/config.php
@@ -90,7 +90,7 @@ $config['base_url'] 				= 'https://voting-system-50f6.onrender.com/';
 | variable to blank.
 |
 */
-$config['index_page']               = 'index.php';
+$config['index_page']               = '';
 
 /*
 |--------------------------------------------------------------------------
